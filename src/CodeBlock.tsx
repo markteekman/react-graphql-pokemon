002cf2364bdde8340c48c27f386c08d851718de8
Mark Teekman <markteekman@users.noreply.github.com>
2025-07-09 15:55:45 +0200

    Remove unused import of useState in CodeBlock component to streamline code and improve performance.

--- src/CodeBlock.tsx
+++ src/CodeBlock.tsx
@@ -1,4 +1,3 @@
-import { useState } from 'react';
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 import { vscDarkPlus } from 'react-syntax-highlighter/dist/esm/styles/prism';
 
